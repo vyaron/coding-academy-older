6799ac75d1b09a374548da4c05996e098314a54f
vyaron <vyaron@gmail.com>
2015-09-28 20:29:35 +0300

    favicon

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="shortcut icon" href="favicon.png"  />
+
 
       <title>Coding Academy | להתחיל לתכנת</title>
 
